8bd1c9816c442e57d00845253e7f9a01311a0dd9
Andreas Schmitz <schmitz@terrestris.de>
2024-11-19 10:05:28 +0100

    feat: initialize backend

---
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/MdeBackendApplication.java
@@ -0,0 +1,22 @@
+package de.terrestris.mde.mde_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Configuration
+@PropertySource(value = "classpath:/application.properties", encoding = "UTF8")
+@PropertySource(ignoreResourceNotFound = true, value = "file:///config/application.properties", encoding = "UTF8")
+public class MdeBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MdeBackendApplication.class, args);
+  }
+
+}
