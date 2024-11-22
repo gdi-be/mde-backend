06a2361b4194eeddc78849cdeef35eb1a436261c
Kai Volland <kaivolland@yahoo.de>
2024-11-22 10:52:42 +0100

    feat: add oauth handling

--- mde-services/src/main/java/de/terrestris/mde/mde_backend/config/WebSecurityConfig.java
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package de.terrestris.mde.mde_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,15 +10,62 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfig {
 
+    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {}
+
+    @Bean
+    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+        return claims -> {
+            var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
+            var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
+            return roles.map(List::stream)
+                    .orElse(Stream.empty())
+                    .map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast)
+                    .toList();
+        };
+    }
+
+    @Bean
+    public JwtAuthenticationConverter authenticationConverter(
+            Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter) {
+        var authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> authoritiesConverter.convert(jwt.getClaims()));
+        return authenticationConverter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.oauth2ResourceServer(resourceServer -> {
+            resourceServer.jwt(jwtDecoder -> {
+                jwtDecoder.jwtAuthenticationConverter(authenticationConverter(realmRolesAuthoritiesConverter()));
+            });
+        });
+
+        http.sessionManagement(sessions -> {
+            sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }).csrf(csrf -> {
+            csrf.disable();
+        });
+
+        // TODO: Reenable CSRF
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
--- mde-services/src/main/java/de/terrestris/mde/mde_backend/controller/MetadataController.java
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/controller/MetadataController.java
@@ -1,7 +1,11 @@
 package de.terrestris.mde.mde_backend.controller;
 
 import de.terrestris.mde.mde_backend.model.ClientMetadata;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Log4j2
 @Controller
 @RequestMapping("/metadata")
 public class MetadataController {
@@ -30,7 +35,13 @@ public class MetadataController {
         }
 
         @GetMapping("/{id}")
-        public ResponseEntity<ClientMetadata> getById(@PathVariable BigInteger id) {
+        public ResponseEntity<ClientMetadata> getById(JwtAuthenticationToken auth, @PathVariable BigInteger id) {
+
+            String username = auth.getToken().getClaimAsString(StandardClaimNames.PREFERRED_USERNAME);
+            List<String> authorities = auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+
+            log.info("User {} with authorities {} requested metadata with id {}", username, authorities, id);
+
             ClientMetadata clientMetadata = new ClientMetadata();
             clientMetadata.setId(id);
             clientMetadata.setTitle("Metadata Title");
