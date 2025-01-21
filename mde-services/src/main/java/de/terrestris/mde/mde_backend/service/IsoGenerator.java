1e1227bf79b308db5ea2cacb054489ed112d6783
Andreas Schmitz <schmitz@terrestris.de>
2025-01-21 14:11:21 +0100

    feat: add variable replacement when generating metadata

--- mde-services/src/main/java/de/terrestris/mde/mde_backend/service/IsoGenerator.java
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/service/IsoGenerator.java
@@ -1,12 +1,16 @@
 package de.terrestris.mde.mde_backend.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.terrestris.mde.mde_backend.jpa.IsoMetadataRepository;
+import de.terrestris.mde.mde_backend.utils.MdeException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.xml.stream.XMLStreamException;
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 @Log4j2
@@ -17,6 +21,23 @@ public class IsoGenerator {
 
   @Autowired
   private DatasetIsoGenerator datasetIsoGenerator;
+
+  private static final Map<String, String> VALUES_MAP;
+
+  static {
+    try {
+      VALUES_MAP = new ObjectMapper().readValue(new File(System.getenv("VARIABLE_FILE")), Map.class);
+    } catch (IOException e) {
+      throw new MdeException("Could not read the variables map file.", e);
+    }
+  }
+
+  public static String replaceValues(String text) {
+    for (var entry : VALUES_MAP.entrySet()) {
+      text = text.replace(entry.getKey(), entry.getValue());
+    }
+    return text;
+  }
 
   public String generateMetadata(String metadataId) throws XMLStreamException, IOException {
     var metadata = isoMetadataRepository.findByMetadataId(metadataId);
---
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/utils/MdeException.java
@@ -0,0 +1,9 @@
+package de.terrestris.mde.mde_backend.utils;
+
+public class MdeException extends RuntimeException {
+
+  public MdeException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
