c813348dc1b95771cf02c03a238c04d3672b05ca
Kai Volland <kaivolland@yahoo.de>
2025-01-28 16:06:01 +0100

    feat: reindex search items on startup

--- mde-services/src/main/java/de/terrestris/mde/mde_backend/service/SearchService.java
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/service/SearchService.java
@@ -2,11 +2,13 @@ package de.terrestris.mde.mde_backend.service;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Log4j2
 @Service
 public class SearchService {
 
@@ -16,9 +18,11 @@ public class SearchService {
   @Transactional(readOnly = true)
   public void reindexAll() {
     SearchSession searchSession = Search.session(entityManager);
+    log.info("Reindexing started");
     try {
       searchSession.massIndexer()
         .startAndWait();
+      log.info("Reindexing finished");
     } catch (InterruptedException e) {
       throw new RuntimeException("Reindexing failed", e);
     }
