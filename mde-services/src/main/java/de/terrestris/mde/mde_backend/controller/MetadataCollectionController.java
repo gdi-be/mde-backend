13fc2bcafb10ecf0af7a2421f34caab6070a29c9
Kai Volland <kaivolland@yahoo.de>
2025-04-23 16:41:05 +0200

    feat: add interface to serach for lineages

--- mde-services/src/main/java/de/terrestris/mde/mde_backend/controller/MetadataCollectionController.java
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/controller/MetadataCollectionController.java
@@ -6,6 +6,7 @@ import de.terrestris.mde.mde_backend.model.MetadataCollection;
 import de.terrestris.mde.mde_backend.model.dto.*;
 import de.terrestris.mde.mde_backend.model.json.Comment;
 import de.terrestris.mde.mde_backend.model.json.Layer;
+import de.terrestris.mde.mde_backend.model.json.Lineage;
 import de.terrestris.mde.mde_backend.model.json.Service;
 import de.terrestris.mde.mde_backend.service.*;
 import de.terrestris.mde.mde_backend.thread.TrackedTask;
@@ -468,6 +469,43 @@ public class MetadataCollectionController extends BaseMetadataController<Metadat
       return result.hits();
     } catch (Exception e) {
       log.error("Error while searching for MetadataCollection with searchTerm: {}, offset: {}, limit: {}", searchTerm, offset, limit, e);
+
+      throw new ResponseStatusException(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        messageSource.getMessage(
+          "BASE_CONTROLLER.INTERNAL_SERVER_ERROR",
+          null,
+          LocaleContextHolder.getLocale()
+        ),
+        e
+      );
+    }
+  }
+
+  @GetMapping(
+    path = "searchLineage",
+    produces = {
+      "application/json"
+    }
+  )
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(security = { @SecurityRequirement(name = "bearer-key") })
+  @ApiResponses(value = {
+    @ApiResponse(
+      responseCode = "200",
+      description = "Ok: The entity was successfully updated"
+    ),
+    @ApiResponse(
+      responseCode = "500",
+      description = "Internal Server Error: Something internal went wrong while updating the entity"
+    )
+  })
+  public List<Lineage> searchLineage(@RequestParam String searchTerm, @RequestParam String property) {
+    log.trace("Search request for Lineage with searchTerm: {}", searchTerm);
+    try {
+      return this.service.searchLineage(searchTerm, property);
+    } catch (Exception e) {
+      log.error("Error while searching for MetadataCollection with searchTerm: {}", searchTerm);
 
       throw new ResponseStatusException(
         HttpStatus.INTERNAL_SERVER_ERROR,
