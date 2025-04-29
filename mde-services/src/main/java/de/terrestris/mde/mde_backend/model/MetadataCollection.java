ab2cf6c51d78c17e328d7a6598937dcc4e9b15a9
Kai Volland <kaivolland@yahoo.de>
2025-04-29 14:02:43 +0200

    feat: adds clonedFromId and removes properties on clone

--- mde-services/src/main/java/de/terrestris/mde/mde_backend/model/MetadataCollection.java
+++ mde-services/src/main/java/de/terrestris/mde/mde_backend/model/MetadataCollection.java
@@ -22,7 +22,13 @@ import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordFie
 @Data
 public class MetadataCollection extends BaseMetadata {
 
-  @Column @Setter private String metadataId;
+  @Column
+  @Setter
+  private String metadataId;
+
+  @Column
+  @Setter
+  private String clonedFromId;
 
   @Formula("(iso_metadata->>'title')")
   private String title;
@@ -40,9 +46,15 @@ public class MetadataCollection extends BaseMetadata {
   @KeywordField(name = "team_member_sort", sortable = Sortable.YES)
   private Set<String> teamMemberIds;
 
-  @Column @Setter @KeywordField private String ownerId;
+  @Column
+  @Setter
+  @KeywordField
+  private String ownerId;
 
-  @Column @Setter @KeywordField private String assignedUserId;
+  @Column
+  @Setter
+  @KeywordField
+  private String assignedUserId;
 
   @Column
   @Setter
---
+++ mde-services/src/main/resources/db/migration/V2.0.6__cloned_from_id_column.sql
@@ -0,0 +1,1 @@
+ALTER TABLE metadata_collection ADD COLUMN cloned_from_id text;
