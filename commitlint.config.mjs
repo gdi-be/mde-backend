39a4257784eaa3f3df8c78b534997a52976e0c86
Andreas Schmitz <schmitz@terrestris.de>
2026-02-09 12:58:50 +0100

    fix: fix renovate config

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,7 @@
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
-  headerCase: ['lower-case', 'sentence-case'],
-  subjectCase: ['lower-case', 'sentence-case']
+  headerCase: ['lower-case', 'sentence-case', 'start-case'],
+  subjectCase: ['lower-case', 'sentence-case', 'start-case']
 };
 
 export default Configuration;
